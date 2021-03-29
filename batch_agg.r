13584d6612aa32b47eb63a18f8e25e02d4e3ed87
Mihai <mihai.croicu@pcr.uu.se>
2021-03-29 12:18:33 +0200

    aggregator

--- batch_agg.r
+++ batch_agg.r
@@ -1,17 +1,22 @@
 library(tidyverse)
+library(jsonlite)
 
 linii <- c()
 bar <- data.frame()
 
-i=3
-cur_bar <- tryCatch({
+for (i in 1:8) {
+    cur_bar <- tryCatch({
         read_csv(paste0('bar_extras',i,'.csv'))
         },
         error=function(cond){
           head(tibble(linie=NA),0)
         })
+    cur_bar <- cur_bar %>% filter(!linie %in% linii)
+    linii_noi <- (cur_bar%>%select(linie)%>%unique())[['linie']]
+    linii <- c(linii, linii_noi)
+    bar <- rbind(bar, cur_bar)
+}
 
-cur_bar <- cur_bar %>% filter(!linie %in% linii)
-linii_noi <- (cur_bar%>%select(linie)%>%unique())[['linie']]
-linii <- c(linii, linii_noi)
-bar <- rbind(bar, cur_bar)
+write_json(bar, 'bar_final.json')
+write_csv(bar, 'bar_final.csv')
+
